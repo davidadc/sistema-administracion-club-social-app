a8bb288bfa7addc790e8240b3fff229373fc1f7b
Juan Gòmez <juangszoke@gmail.com>
2021-03-09 12:17:29 -0400

    menu bug

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin.routing.module';
+import { MenuComponent } from './components/menu/menu.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
-  declarations: [],
+  declarations: [MenuComponent],
   imports: [
     CommonModule,
+    IonicModule,
     AdminRoutingModule
-  ]
+  ],
 })
 export class AdminModule { }
--- src/app/modules/admin/admin.routing.module.ts
+++ src/app/modules/admin/admin.routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: AdminComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
   }
 ];
 
