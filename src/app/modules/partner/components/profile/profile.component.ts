0fd41c2163c19ea5008b3a509c98831f35d72238
Ivan Fuentes <ivalex.97.01@gmail.com>
2021-03-20 06:48:55 -0400

    Use edit profile view to update user information.

--- src/app/modules/partner/components/profile/profile.component.ts
+++ src/app/modules/partner/components/profile/profile.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { PartnerService } from '../../partner.service';
 
 @Component({
@@ -9,109 +7,32 @@ import { PartnerService } from '../../partner.service';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  
-  public editValues:boolean = false
-  private user;  
-  public editProfileForm: FormGroup;
+  private user;
+  private profileType: string = "";
 
-  constructor(
-    private fb: FormBuilder,
-    private partnerService: PartnerService,
-    private router: Router
-  ) {}
+  constructor(private partnerService: PartnerService) {}
 
   ngOnInit() {
-    this.initializeForm();
+  }
+
+  ionViewWillEnter() {
     this.partnerService.getUserData().subscribe((data: any) => {
       if (data) {
         this.user = data.data;
-        this.editProfileForm.get("name").setValue(this.user.name);
-        this.editProfileForm.get("email").setValue(this.user.email);
-        this.editProfileForm.get("phone").setValue(this.user.phone);
+        if (this.user.partner && this.user.partner.qualification) {
+          switch (this.user.partner.qualification) {
+            case 1:
+              this.profileType = "Bronce";
+              break;
+            case 2:
+              this.profileType = "Plata";
+              break;
+            case 3:
+              this.profileType = "Oro";
+              break;
+          }
+        }
       }
     });
   }
-
-  editDates() {
-    this.editValues=!this.editValues;
-    if(this.editValues) {
-      this.editProfileForm.get('name').enable();
-      this.editProfileForm.get('email').enable();
-      this.editProfileForm.get('phone').enable();
-      this.editProfileForm.get('password').enable();
-      this.editProfileForm.get('confirmPassword').enable();
-    } else {
-      this.editProfileForm.get('name').disable();
-      this.editProfileForm.get('email').disable();
-      this.editProfileForm.get('phone').disable();
-    }
-  }
-
-  initializeForm(): void {
-    this.editProfileForm = this.fb.group({
-      name: [{value:"", disabled: true}, Validators.required],
-      email: [{value:"", disabled: true}, [Validators.required, Validators.email]],
-      phone: [{value:"", disabled: true},  Validators.required],
-      password: [
-        {value:"", disabled: true},
-        [
-          Validators.required,
-          Validators.pattern(
-            "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
-          ),
-        ],
-      ],
-      confirmPassword: [
-        {value:"", disabled: true},
-        [
-          Validators.required,
-          Validators.pattern(
-            "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
-          ),
-        ],
-      ],
-    });
-  }
-
-  get name() {
-    return this.editProfileForm.get("name");
-  }
-
-  get phone() {
-    return this.editProfileForm.get("phone");
-  }
-
-  get email() {
-    return this.editProfileForm.get("email");
-  }
-
-  get password() {
-    return this.editProfileForm.get("password");
-  }
-
-  get confirmPassword() {
-    return this.editProfileForm.get("confirmPassword");
-  }
-
-  get confirmPasswordValue() {
-    return (
-      this.editProfileForm.get("password").value ==
-      this.editProfileForm.get("confirmPassword").value
-    );
-  }
-
-  updateUser() {
-    this.partnerService
-      .updateUser(this.editProfileForm.value)
-      .subscribe((data) => {
-        console.log(data);
-        this.editValues= !this.editValues;
-        this.editProfileForm.get('name').disable();
-        this.editProfileForm.get('email').disable();
-        this.editProfileForm.get('phone').disable();
-        this.router.navigate(["/partner/profile"]);
-      });
-  }
-
-
 }
